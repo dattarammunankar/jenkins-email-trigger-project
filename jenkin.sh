1f07fd8456b364f0f1a3722bc2be0a84cb7abdb5
dattarammunankar <dattarammunankar@gmail.com>
2026-01-28 22:21:55 +0530

    Update jenkin.sh

--- jenkin.sh
+++ jenkin.sh
@@ -3,4 +3,4 @@ echo "========================================="
 echo "   JENKINS AUTOMATION TASK-2 SUCCESS     "
 echo "   Executed on: $(date)"
 echo "========================================="
-
+task22
