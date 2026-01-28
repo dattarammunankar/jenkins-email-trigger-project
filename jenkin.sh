6d4d8709d069028b9da2336b9af84dc8ff60b4b4
dattarammunankar <dattarammunankar@gmail.com>
2026-01-28 20:44:18 +0530

    Create jenkin.sh

---
+++ jenkin.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "========================================="
+echo "   JENKINS AUTOMATION TASK-2 SUCCESS     "
+echo "   Executed on: $(date)"
+echo "========================================="
